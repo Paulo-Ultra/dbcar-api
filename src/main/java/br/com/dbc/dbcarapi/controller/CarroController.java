146ca8c11e8e28872b1ab95928045bb4ae557baa
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2022-07-15 16:38:16 -0300

    Ajuste para teste de conexão

--- src/main/java/br/com/dbc/dbcarapi/controller/CarroController.java
+++ src/main/java/br/com/dbc/dbcarapi/controller/CarroController.java
@@ -1,4 +1,28 @@
 package br.com.dbc.dbcarapi.controller;
 
+import br.com.dbc.dbcarapi.entity.Carro;
+import br.com.dbc.dbcarapi.exception.BancoDeDadosException;
+import br.com.dbc.dbcarapi.service.CarroService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/carro")
+@Validated
 public class CarroController {
+
+    @Autowired
+    private CarroService carroService;
+
+    @GetMapping
+    public ResponseEntity<List<Carro>> list() throws BancoDeDadosException {
+        return new ResponseEntity<>(carroService.list(), HttpStatus.OK);
+    }
 }
--- src/main/java/br/com/dbc/dbcarapi/repository/Repositories.java
+++ src/main/java/br/com/dbc/dbcarapi/repository/Repositories.java
@@ -1,12 +1,9 @@
 package br.com.dbc.dbcarapi.repository;
 
-import br.com.dbc.dbcarapi.entity.Carro;
-import br.com.dbc.dbcarapi.exception.BancoDeDadosException;
-
 import java.util.List;
-import java.util.Optional;
 
-public interface Repository <CHAVE, OBJECT> {
+
+public interface Repositories<CHAVE, OBJECT> {
     List<OBJECT> findAll();
     OBJECT create(OBJECT object);
     OBJECT update(CHAVE id, OBJECT object);
