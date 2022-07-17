6d82c84b9d18fa9f346a41df89d408cbc2f13787
Willian Valentim <williann.valentim@gmail.com>
2022-07-17 11:00:38 -0300

    Alterações na entidade aluguel

--- src/main/java/br/com/dbc/dbcarapi/entity/Aluguel.java
+++ src/main/java/br/com/dbc/dbcarapi/entity/Aluguel.java
@@ -15,41 +15,10 @@ import java.util.Objects;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Aluguel {
+
     private Integer idAluguel;
     private Integer idCliente;
     private Integer idCarro;
     private LocalDate diaDoAluguel;
     private LocalDate diaDaEntrega;
-
-//    public Long diasComCarro() {
-//        return diaDaEntrega.until(diaDoAluguel, ChronoUnit.DAYS);
-//    }
-//
-//    public Double calcularDiaria() {
-//        if (carro.getClasse().equals(ClasseCarro.A.getTipo())) {
-//            return diasComCarro() * carro.getPrecoDiaria() * 1.5;
-//        } else if (carro.getClasse().equals(ClasseCarro.B.getTipo())) {
-//            return diasComCarro() * carro.getPrecoDiaria() * 1.2;
-//        } else if (carro.getClasse().equals(ClasseCarro.C.getTipo())) {
-//            return diasComCarro() * carro.getPrecoDiaria();
-//        } else {
-//            System.out.println("O carro informado não está disponível.");
-//            return null;
-//        }
-//    }
-//
-//    @Override
-//    public String toString() {
-//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-//        DecimalFormat df = new DecimalFormat("#,###.00");
-//        return "{"
-//                + "\"idAluguel\": " + getIdAluguel() + ", "
-//                + "\"Carro\": " + getCarro() + ", "
-//                + "\"Cliente\": " + getCliente() + ", "
-//                + "\"Dia do Aluguel\": \"" + getDiaDoAluguel().format(formatter) + "\", "
-//                + "\"Dia da Entrega\": \"" + getDiaDaEntrega().format(formatter) + "\", "
-//                + "\"Dias\": " + diasComCarro() + ", "
-//                + "\"Diária\": \"" + df.format(calcularDiaria())
-//                + "\"}";
-//    }
 }
